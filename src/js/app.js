7daf67de997147e0ea19a1f331c0814a8def0f25
prilipkoleg <prilipkoleg@gmail.com>
2015-10-08 10:15:57 +0300

    add 2 types of graph

--- src/js/app.js
+++ src/js/app.js
@@ -7,58 +7,83 @@ var source = audioCtx.createMediaElementSource(audio);
 source.connect(analyser);
 analyser.connect(audioCtx.destination);
 
-var canvas = document.getElementById("myCanvas");
-var canvasCtx = canvas.getContext("2d");
+var oscilloscope = document.getElementById("oscilloscope");
+var frequency_bar = document.getElementById("frequency_bar");
 
-//a waveform/oscilloscope
-analyser.fftSize = 256;
-var bufferLength = analyser.frequencyBinCount;
-console.log(bufferLength);
-var dataArray = new Uint8Array(bufferLength);
+var canvasOsCtx = oscilloscope.getContext("2d");
+var canvasFBCtx = frequency_bar.getContext("2d");
 
 var WIDTH   = 500,
     HEIGHT  = 200;
 
-canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+//a waveform/oscilloscope
+analyser.fftSizeOs = 2048;
+var bufferLengthOs = analyser.frequencyBinCount;
+var dataArrayOs = new Uint8Array(bufferLengthOs);
 
-function draw() {
-    drawVisual = requestAnimationFrame(draw);
-    analyser.getByteFrequencyData(dataArray);
+canvasOsCtx.clearRect(0, 0, WIDTH, HEIGHT);
 
-    canvasCtx.fillStyle = 'rgb(200, 200, 200)';
-    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+function draw_oscilloscope() {
+    drawVisualOs = requestAnimationFrame(draw_oscilloscope);
+    analyser.getByteFrequencyData(dataArrayOs);
 
-    var barWidth = (WIDTH / bufferLength) * 2.5;
+    canvasOsCtx.fillStyle = 'rgb(200, 200, 200)';
+    canvasOsCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+    canvasOsCtx.lineWidth = 2;
+    canvasOsCtx.strokeStyle = 'rgb(0, 0, 0)';
+    canvasOsCtx.beginPath();
+
+    var sliceWidth = WIDTH * 1.0 / bufferLengthOs;
+    var x = 0;
+
+    for(var i = 0; i < bufferLengthOs; i++) {
+
+        var v = dataArrayOs[i] / 128.0;
+        var y = v * HEIGHT/2;
+
+        if(i === 0) {
+            canvasOsCtx.moveTo(x, y);
+        } else {
+            canvasOsCtx.lineTo(x, y);
+        }
+
+        x += sliceWidth;
+    }
+
+    canvasOsCtx.lineTo(oscilloscope.width, oscilloscope.height/2);
+    canvasOsCtx.stroke();
+};
+
+// a frequency bar graph
+analyser.fftSizeFB = 256;
+var bufferLengthFB = analyser.frequencyBinCount;
+var dataArrayFB = new Uint8Array(bufferLengthFB);
+
+canvasFBCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+function draw_frequency_bar() {
+    drawVisualFB = requestAnimationFrame(draw_frequency_bar);
+    analyser.getByteFrequencyData(dataArrayFB);
+
+    canvasFBCtx.fillStyle = 'rgb(200, 200, 200)';
+    canvasFBCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+    var barWidth = (WIDTH / bufferLengthFB) * 2.5;
     var barHeight;
     var x = 0;
 
-    canvasCtx.beginPath();
+    canvasFBCtx.beginPath();
 
-    for(var i = 0; i < bufferLength; i++) {
-        barHeight = dataArray[i]/2;
+    for(var i = 0; i < bufferLengthFB; i++) {
+        barHeight = dataArrayFB[i]/2;
 
-        canvasCtx.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
-        canvasCtx.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight);
+        canvasFBCtx.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
+        canvasFBCtx.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight);
 
         x += barWidth + 1;
     }
 };
 
-draw();
-/*
-//a frequency bar graph
-analyser.fftSize = 256;
-var bufferLength = analyser.frequencyBinCount;
-console.log(bufferLength);
-var dataArray = new Uint8Array(bufferLength);
-
-canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
-
-function draw() {
-    drawVisual = requestAnimationFrame(draw);
-
-    analyser.getByteFrequencyData(dataArray);
-
-    canvasCtx.fillStyle = 'rgb(0, 0, 0)';
-    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-}*/
+draw_oscilloscope();
+draw_frequency_bar();
